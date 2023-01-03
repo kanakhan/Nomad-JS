6e501fa427eb92c2942e481c14f836a99b77988e
kanakhan <cjh1950@naver.com>
2023-01-03 23:39:15 +0900

    const and let

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("lalala")
+let a = 5;
+const b = 2;
+const myName = "Jihwan";
+//const -> 상수, let -> 변수
+
+a=2;
+
+console.log("a =", a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("my name is "+"\n"+myName);
