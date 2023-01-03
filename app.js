fb904ef3f11add39cc3d6cbe4addf14b6fbceffe
kanakhan <cjh1950@naver.com>
2023-01-04 01:31:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-let a = 5;
-const b = 2;
-const myName = "Jihwan";
-//const -> 상수, let -> 변수
-
-a=2;
-
-console.log("a =", a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("my name is "+"\n"+myName);
+const amIFat = false;
+console.log(amIFat);
